42e23f1b13456e91249eaf83b1159552957b7216
Mike 'mitch' Mitchel <mike@mikemitchel.com>
2020-05-15 11:13:56 -0500

    remove sample urls from build script

--- build.production.html.js
+++ build.production.html.js
@@ -40,33 +40,33 @@ function template (version) {
       <script>
         localStorage.setItem('a2jConfig', JSON.stringify({
           // path (or url) to the interview XML file
-          templateURL: '../../A2JGetInterview.aspx?data=2417^b4e01b9c-4573-4776-b195-cb9490ac0c3b^1c725dd1-8bf6-4dc5-955d-b0cfb94600a5',
+          templateURL: '',
 
           // folder containing images, templates and other assets related to the interview
           fileDataURL: 'http://localhost/54583/Assets/A2J_Modules/New York/Vacate_Judgment_Consumer_Debt_2417/',
 
           // endpoint to load an answer file at start, used for RESUME
-          getDataURL: 'A2JGetData.aspx?data=0^b4e01b9c-4573-4776-b195-cb9490ac0c3b^2417^dcb160b4-7321-44c5-a99a-92d3e3efc1d6',
+          getDataURL: '',
 
           // endpoint to save the answer's file and leaves the viewer (its response replaces viewer's frame)
-          setDataURL: '../Engine/Index?q=6ick6e1t9CGdhvn+4ZDvkluGkgjfo/WZ9JkfJHXuyVKOBFj8ImxjPEfdygh4isgV7aDXg4hf5pKqRDHkQ/LZh1CBeIp9fuPfDT3jRXSaxrMEcuLrOVKIOfIyu6ceh9gTGkkbNFjDhQ+sLDrKt2jRnDaxpszkvXjFhhgqNtqM9V37wMgJq72zEoRPIUbEjRwHPg6i8g812pzK9gFIRHkVHcVAhM/dWQsqof9bXQ3azmANnQz6yXw1rJ75RpjlJ+KY7UGCwLxu86a5fLHzXWLwbg==',
+          setDataURL: '',
 
           // (Optional) ajax endpoit to silently save the answer file periodically
-          autoSetDataURL: 'A2J_ViewerAutoSetData.php?data=ExtraData4autoSetDataURL',
+          autoSetDataURL: '',
 
           // use to replace the viewer's frame on EXIT (user 'fails' interview)
-          exitURL: 'A2J_ViewerExit.html',
+          exitURL: '',
 
           // endpoint to silent log data
           // E.g., https://lawhelpinteractive.org/a2j_logging
-          logURL: 'A2J_ViewerLog.php?data=ExtraData4logURL',
+          logURL: '',
 
           // errRepURL accepts user's public submission of an error to which they can add an optional comment.
           // E.g., 'https://lawhelpinteractive.org/problem_reporting_form'
-          errRepURL: 'A2J_ViewerErrRep.php?data=ExtraData4errRepURL'
+          errRepURL: ''
         }));
       </script>
-      <script src="../node_modules/steal/steal.production.js?v=${version}" cache-key="v" cache-version="${version}" main="a2jviewer/app"></script>
+      <script src="node_modules/steal/steal.production.js?v=${version}" cache-key="v" cache-version="${version}" main="a2jviewer/app"></script>
     </body>
   </html>`;
 }
