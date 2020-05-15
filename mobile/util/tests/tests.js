519cfbcf8f26b7de1c8fa8d9231bfc32945d3c5c
Mike 'mitch' Mitchel <mike@mikemitchel.com>
2020-05-15 12:56:48 -0500

    add mobile util tests runner page

--- mobile/util/tests/tests.js
+++ mobile/util/tests/tests.js
@@ -1,8 +1,5 @@
 import 'a2jviewer/mobile/util/tests/lang-test'
 import 'a2jviewer/mobile/util/tests/logic-test'
-import 'a2jviewer/mobile/util/tests/parser-test'
 import 'a2jviewer/mobile/util/tests/validations-test'
-import 'a2jviewer/mobile/util/tests/string-test'
-import 'a2jviewer/mobile/util/tests/date-test'
 import 'a2jviewer/mobile/util/tests/tlogic-test'
 import 'a2jviewer/mobile/util/tests/helpers-test'
