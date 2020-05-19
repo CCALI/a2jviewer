9e1151c9174d9bb3f5e6063d3cfad6b39fa6c7f3
Mike 'mitch' Mitchel <mike@mikemitchel.com>
2020-05-19 09:48:29 -0500

    update favicon path in build prod script

--- build.production.html.js
+++ build.production.html.js
@@ -18,15 +18,15 @@ function template (version) {
   <html>
     <head>
       <!-- Favicon Image Support from: https://sympli.io/blog/2017/02/15/heres-everything-you-need-to-know-about-favicons-in-2017/-->
-      <link rel="icon" type="image/png" href="../favicons/favicon-16x16.png" sizes="16x16">
-      <link rel="icon" type="image/png" href="../favicons/favicon-32x32.png" sizes="32x32">
-      <link rel="icon" type="image/png" href="../favicons/favicon-96x96.png" sizes="96x96">
+      <link rel="icon" type="image/png" href="favicons/favicon-16x16.png" sizes="16x16">
+      <link rel="icon" type="image/png" href="favicons/favicon-32x32.png" sizes="32x32">
+      <link rel="icon" type="image/png" href="favicons/favicon-96x96.png" sizes="96x96">
 
       <!-- Favicon Image Support for Apple (and Android) -->
-      <link rel="apple-touch-icon" href="../favicons/iPhone-120x120.png"> <!-- 120px -->
-      <link rel="apple-touch-icon" sizes="180x180" href="../favicons/iPhone-180x180.png">
-      <link rel="apple-touch-icon" sizes="152x152" href="../favicons/iPhone-152x152.png">
-      <link rel="apple-touch-icon" sizes="167x167" href="../favicons/iPhone-167x167.png">
+      <link rel="apple-touch-icon" href="favicons/iPhone-120x120.png"> <!-- 120px -->
+      <link rel="apple-touch-icon" sizes="180x180" href="favicons/iPhone-180x180.png">
+      <link rel="apple-touch-icon" sizes="152x152" href="favicons/iPhone-152x152.png">
+      <link rel="apple-touch-icon" sizes="167x167" href="favicons/iPhone-167x167.png">
 
       <title>A2J Viewer</title>
       <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
