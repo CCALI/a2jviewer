d27e5a7e05f16f5826a9ffdcd401994dbee702d3
Anubhav Dhingra <anubhav.dhingra@launchventures.co>
2021-03-31 17:29:48 +0530

    Revert "registered truncateText helper in stache gloablly"
    This reverts commit 2ddb4e398d30bea2c511d394afa6dcc2e58aea5c.

--- src/mobile/util/tests/helpers-test.js
+++ src/mobile/util/tests/helpers-test.js
@@ -1,5 +1,5 @@
 import { assert } from 'chai'
-import { normalizePathHelper, insertExternalLinkIconHelper, truncateTextHelper } from '~/src/mobile/util/helpers'
+import { normalizePathHelper, insertExternalLinkIconHelper } from '~/src/mobile/util/helpers'
 import 'steal-mocha'
 
 describe('helpers module', function () {
@@ -55,31 +55,6 @@ describe('helpers module', function () {
 
       path = '/Users/dev/Sites/file.xml'
       assert.equal(normalizePathHelper(pathToGuide, path), '/userfiles/user/guide/file.xml')
-    })
-  })
-
-  describe('truncateText', function () {
-    it('keeps short text unchanged', () => {
-      let shortText = 'Foo Test'
-      assert.equal(truncateTextHelper(shortText), 'Foo Test', 'should not change short text')
-    })
-
-    it('truncates long texts', function () {
-      let longText = 'slightly longer text with a space as the 51st char that gets truncated'
-      assert.equal(
-        truncateTextHelper(longText),
-        'slightly longer text with a space as the 51st char...',
-        'should truncate to 50 chars and add an ellipsis'
-      )
-    })
-
-    it('truncates the space in after 50th character', function () {
-      let step = 'long text with a space as the 50th character in the middle of a word'
-      assert.equal(
-        truncateTextHelper(step),
-        'long text with a space as the 50th character in...',
-        'should truncate to last full word before 50th char and add an ellipsis'
-      )
     })
   })
 })
