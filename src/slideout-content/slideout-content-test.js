480758ac1536eb40ebba42330a6eeedde6f9d24a
Anubhav Dhingra <anubhav.dhingra@launchventures.co>
2021-04-13 22:26:48 +0530

    navigation panel code cleanup

--- src/slideout-content/slideout-content-test.js
+++ src/slideout-content/slideout-content-test.js
@@ -1,6 +1,10 @@
-// import app-state model
+import { SlideoutContentVM } from './slideout-content'
 import 'steal-mocha'
+import { assert } from 'chai'
 
 describe('<author-slideout-content>', () => {
-
+  it('basic test', () => {
+    let vm = SlideoutContentVM()
+    assert.isTrue(vm instanceof SlideoutContentVM, 'should be an instance of slideout VM')
+  })
 })
--- src/slideout-content/slideout-content.js
+++ src/slideout-content/slideout-content.js
@@ -1,6 +1,4 @@
 import DefineMap from 'can-define/map/map'
-// import CanList from 'can-list'
-// import DefineList from 'can-define/list/list'
 import Component from 'can-component'
 import template from './slideout-content.stache'
 
